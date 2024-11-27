fb12b20be88b525a19a55a90b293e3bccbeb3344
Khushi Walje <141430285+Khushiwalje18@users.noreply.github.com>
2024-11-27 12:00:30 +0530

    Add files via upload

---
+++ operatorsInJava/src/com/tka/taskOperators/Task2.java
@@ -0,0 +1,20 @@
+package com.tka.taskOperators;
+
+public class Task2 {
+	int pre_incrementation(int n1) {
+		return ++n1;
+	}
+
+	int post_incrementation(int n1) {
+		return n1++;
+	}
+
+	int pre_decrementation(int n1) {
+		return --n1;
+	}
+
+	int post_decrementation(int n1) {
+		return n1--;
+	}
+
+}
