48f1f80bd0626513936a5660e08dd7ff856f76a6
Khushi Walje <141430285+Khushiwalje18@users.noreply.github.com>
2025-01-08 12:33:54 +0530

    Add files via upload

---
+++ Abstraction/src/com/tka/classImprementsInterface/B.java
@@ -0,0 +1,6 @@
+package com.tka.classImprementsInterface;
+
+public interface B {
+	int add(int n1, int n2);
+	
+}
---
+++ Abstraction/src/com/tka/classImprementsInterface/TestAB.java
@@ -0,0 +1,9 @@
+package com.tka.classImprementsInterface;
+
+public class TestAB {
+	public static void main(String[] args) {
+		A obj=new A();
+		int res1 = obj.add(34,6);
+		System.out.println("Addition result: "+res1);
+	}
+}
